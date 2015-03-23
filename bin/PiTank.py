0be9e00e180fdaf54cdc30e88fb62af70e4aac7e
Sterling Archer <davidribyrne+github@gmail.com>
2015-03-22 21:43:54 -0600

    updates

--- bin/PiTank.py
+++ bin/PiTank.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0',
                             'server.socket_port': 80,
                             'log.access_file': './logs/access',
-                            'log.error_file': './logs/error',
+                            'log.error_file': './logs/error'
                             })
     WebSocketPlugin(cherrypy.engine).subscribe()
     cherrypy.tools.websocket = WebSocketTool()
