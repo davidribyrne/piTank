03023a5f546a6071a578c6442070bbb8ef1893d3
Sterling Archer <davidribyrne+github@gmail.com>
2015-03-22 21:30:52 -0600

    update

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 # Port 80 & access to GPIO require root
 # Probably a more secure way to do this, but it's a toy, not a production server
 
-sudo python bin/PiTank.py
+mkdir log
+sudo python bin/PiTank.py &> log/server.log
